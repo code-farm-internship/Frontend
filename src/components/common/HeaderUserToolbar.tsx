30934186c7cf856723e7a620c2e142ae7c2fe46e
pbt <tranhphungba8@gmail.com>
2025-06-08 19:41:56 +0700

    feat(header): add responsive for mobile

--- src/components/common/HeaderUserToolbar.tsx
+++ src/components/common/HeaderUserToolbar.tsx
@@ -10,9 +10,9 @@ const HeaderUserToolbar = () => {
     return (
         <>
             <Dropdown menu={{ items: GetUserToolbarItems() }}>
-                <div className='group hidden cursor-pointer items-center p-2 text-gray-600 transition-colors md:flex'>
+                <div className='group flex cursor-pointer items-center p-2 text-gray-600 transition-colors'>
                     <UserOutlined className='text-xl' />
-                    <span className='ml-2 hidden w-20 truncate group-hover:text-primary lg:inline'>
+                    <span className='ml-2 hidden w-20 truncate group-hover:text-primary md:inline'>
                         {userInfo.username || 'Tài khoản'}
                     </span>
                 </div>
